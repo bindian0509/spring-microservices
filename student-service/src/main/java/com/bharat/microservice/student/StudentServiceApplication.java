fcf2c9d987f7ee60e31d4ae7724a7d8f8eb7bf70
Bharat Verma <bindian0509@gmail.com>
2022-07-07 02:10:34 +0530

    Will add Student Service Later

--- student-service/src/main/java/com/bharat/microservice/student/StudentServiceApplication.java
+++ student-service/src/main/java/com/bharat/microservice/student/StudentServiceApplication.java
@@ -1,9 +1,15 @@
 package com.bharat.microservice.student;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@ComponentScan({"com.bharat.microservice.student"})
+@EntityScan("com.bharat.microservice.student.model.entity")
+@EnableJpaRepositories("com.bharat.microservice.student.model.repository")
+@EnableFeignClients("com.bharat.microservice.student.service")
 public class StudentServiceApplication {
 
     public static void main(String[] args) {

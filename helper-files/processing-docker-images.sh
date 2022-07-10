7d4077ab3a7b96de768f80f87031dc36a9bc67a0
Bharat Verma <bindian0509@gmail.com>
2022-07-11 00:49:43 +0530

    Added Docker Image Building from pom.xml

---
+++ helper-files/processing-docker-images.sh
@@ -0,0 +1,9 @@
+cd ../eureka-server/  && mvn spring-boot:build-image
+
+cd ../config-server/  && mvn spring-boot:build-image
+
+cd ../api-gateway/  && mvn spring-boot:build-image
+
+cd ../address-service/  && mvn spring-boot:build-image
+
+cd ../student-service/  && mvn spring-boot:build-image
